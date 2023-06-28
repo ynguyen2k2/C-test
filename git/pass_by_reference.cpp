c4ba86a15f1d72f6c0332a71ec17083f0ef59914
ynguyen2k2 <ynguyen2017tt@gmail.com>
2023-06-29 00:09:43 +0700

    push code

--- git/pass_by_reference.cpp
+++ git/pass_by_reference.cpp
@@ -1,5 +1,15 @@
 // pass by reference
 #include <stdio.h>
+void swap(int *x, int *y) {
+
+   int temp;
+
+   temp = *x; /* save the value of x */
+   *x = *y;    /* put y into x */
+   *y = temp; /* put temp into y */
+  
+   return;
+}
 
 int main () {
 
